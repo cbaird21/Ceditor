535944daed43b115916ecb3a5bc0cd733eed5740
unknown <chandler.baird21@gmail.com>
2023-01-08 16:31:30 -0700

    implemented asset caching

--- client/sw.js
+++ client/sw.js
@@ -27,6 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+const assetCache = new CacheFirst({
+  cacheName: "asset-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+registerRoute(({ request }) => request.destination === "image", assetCache);
 warmStrategyCache({ urls, strategy });
 offlineFallback();
 
