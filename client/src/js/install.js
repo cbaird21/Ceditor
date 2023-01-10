a2039127102f1180dbc752729f3004583db91ede
unknown <chandler.baird21@gmail.com>
2023-01-10 09:18:53 -0700

    fixed install.js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,24 +4,18 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    window.deferredPrompt = event;
-    butInstall.style.display = "block";
+    butInstall.style.visibility = 'visible';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    butInstall.style.display = "none";
-    window.deferredPrompt.prompt();
-    const userChoice = await window.deferredPrompt.userChoice;
-    window.deferredPrompt = null;
-    if (userChoice.outcome === "accepted") {
-        console.log("User wants to install Jate");
-    } else {
-        console.log("User doesn't want to install")
-    }
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'installed!'
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log("Jate has been installed")
+    textHeader.textContent = 'Successfully installed!';
+    console.log("Jate has been installed", 'appinstalled', event)
 });
